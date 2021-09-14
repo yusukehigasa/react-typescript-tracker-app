c8436f0dec4e2f8590d9d71b423897667633ff1d
yusuke higasa <higasa@studio-higasa.com>
2021-09-14 17:04:22 +0900

    Add .catch() to fetch()

--- src/App.tsx
+++ src/App.tsx
@@ -37,13 +37,15 @@ function App() {
           newRecovered: data[data.length - 1].Recovered - data[data.length - 2].Recovered,
           totalRecovered: data[data.length - 1].Recovered,
         });
-      });
+      })
+      .catch(err => alert('エラーが発生しました。ページをリロードして、もう一度トライしてください。'));
   }
 
   useEffect(() => {
     fetch(`https://api.covid19api.com/summary`)
       .then(res => res.json())
-      .then(data => setAllCountriesData(data.Countries));
+      .then(data => setAllCountriesData(data.Countries))
+      .catch(err => alert('エラーが発生しました。ページをリロードして、もう一度トライしてください。'));
   }, []);
 
   return (
